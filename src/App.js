6c3b66cb266ebb7fd9c5825c3a4b4ba3b1265e78
Martynas Druteika <martynasdd@gmail.com>
2022-10-10 18:21:48 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Codeacademy.lt React paskaita!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://codeacademy.lt/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Codeacademy.lt
         </a>
       </header>
     </div>
